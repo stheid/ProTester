12bcb0b68d13f5f55a582cc5ba1a2fd366224db5
Stefan Heid <stefan-heid@web.de>
2015-01-09 13:09:20 +0100

    Old definition in Maincontroller removed
    View Result Tab fixed

--- server/view/tabs/ViewResultTab.php
+++ server/view/tabs/ViewResultTab.php
@@ -1,51 +1,53 @@
 <?php
-include_once 'Tab.php';
 class ViewResultTab extends Tab {
-	public $id = "view_res";
-	public $title = "View Results";
-	// TODO
-	public $content = '<h1>November 2014</h1>
+	function __construct() {
+		$this->content = '<h1>November 2014</h1>
 <ul class="list-group">
-<li class="list-group-item"><a href="'.PATH.'server/view/viewResult.php">11.
+<li class="list-group-item"><a href="' . PATH . 'server/view/viewResult.php">11.
 Nov - Software System Development Exam<span style="float: right">35/40</span>
 </a></li>
-<li class="list-group-item"><a href="'.PATH.'server/view/viewResult.php">11.
+<li class="list-group-item"><a href="' . PATH . 'server/view/viewResult.php">11.
 Nov - Software System Development Exam<span style="float: right">35/40</span>
 </a></li>
-<li class="list-group-item"><a href="'.PATH.'server/view/viewResult.php">11.
+<li class="list-group-item"><a href="' . PATH . 'server/view/viewResult.php">11.
 Nov - Software System Development Exam<span style="float: right">35/40</span>
 </a></li>
-<li class="list-group-item"><a href="'.PATH.'server/view/viewResult.php">11.
+<li class="list-group-item"><a href="' . PATH . 'server/view/viewResult.php">11.
 Nov - Software System Development Exam<span style="float: right">35/40</span>
 </a></li>
-<li class="list-group-item"><a href="'.PATH.'server/view/viewResult.php">11.
+<li class="list-group-item"><a href="' . PATH . 'server/view/viewResult.php">11.
 Nov - Software System Development Exam<span style="float: right">35/40</span>
 </a></li>
-<li class="list-group-item"><a href="'.PATH.'server/view/viewResult.php">11.
+<li class="list-group-item"><a href="' . PATH . 'server/view/viewResult.php">11.
 Nov - Software System Development Exam<span style="float: right">35/40</span>
 </a></li>
 </ul>
 
 <h1>October 2014</h1>
 <ul class="list-group">
-<li class="list-group-item"><a href="'.PATH.'server/view/viewResult.php">11.
+<li class="list-group-item"><a href="' . PATH . 'server/view/viewResult.php">11.
 Nov - Software System Development Exam<span style="float: right">35/40</span>
 </a></li>
-<li class="list-group-item"><a href="'.PATH.'server/view/viewResult.php">11.
+<li class="list-group-item"><a href="' . PATH . 'server/view/viewResult.php">11.
 Nov - Software System Development Exam<span style="float: right">35/40</span>
 </a></li>
-<li class="list-group-item"><a href="'.PATH.'server/view/viewResult.php">11.
+<li class="list-group-item"><a href="' . PATH . 'server/view/viewResult.php">11.
 Nov - Software System Development Exam<span style="float: right">35/40</span>
 </a></li>
-<li class="list-group-item"><a href="'.PATH.'server/view/viewResult.php">11.
+<li class="list-group-item"><a href="' . PATH . 'server/view/viewResult.php">11.
 Nov - Software System Development Exam<span style="float: right">35/40</span>
 </a></li>
-<li class="list-group-item"><a href="'.PATH.'server/view/viewResult.php">11.
+<li class="list-group-item"><a href="' . PATH . 'server/view/viewResult.php">11.
 Nov - Software System Development Exam<span style="float: right">35/40</span>
 </a></li>
-<li class="list-group-item"><a href="'.PATH.'server/view/viewResult.php">11.
+<li class="list-group-item"><a href="' . PATH . 'server/view/viewResult.php">11.
 Nov - Software System Development Exam<span style="float: right">35/40</span>
 </a></li>
 </ul>';
+	}
+	public $id = "view_res";
+	public $title = "View Results";
+	// TODO
+	public $content;
 }
 ?>
