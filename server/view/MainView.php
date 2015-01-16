458bfd59a3d171529c1a7b555497fd35115b208d
Stefan Heid <stefan-heid@web.de>
2015-01-16 09:53:24 +0100

    recreated Mainview

---
+++ server/view/MainView.php
@@ -0,0 +1,67 @@
+<?php
+include_once 'View.php';
+class MainView extends View{
+	
+	static $tabs;
+	protected static $title='Main';
+	
+	static protected function includes(){
+		parent::includes();
+		include 'Tabs.php';
+	}
+	
+	static protected function content(){
+		MainView::$tabs [] = new NewsTab ();
+		
+		$isStudent = true;
+		if ($isStudent) {
+			MainView::$tabs [] = new ViewResultTab ();
+		}
+		
+		$isLecturer = true;
+		if ($isLecturer) {
+			MainView::$tabs [] = new TestManagementTab ();
+			MainView::$tabs [] = new EvaluateTestTab ();
+		}
+		
+		$isAdmin = false;
+		if ($isAdmin) {
+			MainView::$tabs [] = new CourseManagementTab ();
+			MainView::$tabs [] = new AccountManagementTab ();
+		}
+		
+		$navtabs = '
+			<ul class="nav nav-tabs" role="tablist">';
+		$i = 0;
+		foreach ( MainView::$tabs as $tab ) {
+		
+			$navtabs = $navtabs . '
+				<li role="presentation"' . ($i == 0 ? ' class="active"' : '') . '>
+					<a href="#' . $tab->id . '" aria-controls="' . $tab->title . '" role="tab" data-toggle="tab">' . $tab->title . '
+					</a>
+				</li>';
+			$i ++;
+		}
+		$navtabs = $navtabs . "
+			</ul>";
+		
+		$navpanes = '<div class="tab-content">';
+		$i = 0;
+		foreach ( MainView::$tabs as $tab ) {
+			$navpanes = $navpanes . '
+				<div role="tabpanel" class="tab-pane' . ($i == 0 ? ' active' : '') . '" id="' . $tab->id . '">
+						' . $tab->content . '
+				</div>';
+			$i ++;
+		}
+		
+		$navpanes = $navpanes . '</div>';
+		
+		echo '<div class="container">
+		<div role="tabpanel">' . $navtabs . $navpanes . '</div>
+	</div>';
+	}
+}
+
+new MainView();
+?>
