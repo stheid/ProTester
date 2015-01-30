d24a7bbe793051b57cc0c49c4eb3f74b79cea693
voidcurser <giovannyothoniel@hotmail.com>
2015-01-30 12:01:53 +0100

    login check

--- server/controller/LoginController.php
+++ server/controller/LoginController.php
@@ -1,31 +1,46 @@
 <?php
-require_once(realpath(dirname(__FILE__)) . '/Controller.php');
+require_once(realpath(dirname(__FILE__)) . '/settings.php');
 
+class LoginController{	
+$server = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $database = "ProTester";
+     
+    $db_handle = mysql_connect($server, $username, $password);
+    $db_found = mysql_select_db($database, $db_handle);
+if ($db_found) {
+echo 'Connected successfully';
 
-class LoginController extends Controller {	
-	public static function main(){
-		parent::includes();
-		
-		if (static::hasPermission($_POST["usr"],$_POST["pwd"])){
-			if (static::personHasTests()) {
-				$target=PATH."server/view/TestRunnerView.php";
-			}else {
-				$target=PATH."server/view/MainView.php";
-			}
-		} else {
-			Session::$loginError="You could not be logged in, Try again";
-			$target=PATH."server/view/LoginView.php";
-		}
-		header("Location: $target");
-	}
-	
+session_start();
 	public static function hasPermission($user, $password){
+	 
+	$query=mysqli_query("select from person where ID='".$_POST['username']."' and pwr='".$_POST['password']."'");
+	if(mysqli_num_rows($query)==1){ 
+echo 'Connected LOGN successfully';
+
+} 
 		return false;
 	}
 
 	public static function personHasTests(){
 		return false;
 	}
+
+
+if (LoginController::hasPermission($_POST["usr"],$_POST["pwd"])){
+	if (LoginController::personHasTests()) {
+		$target=PATH."server/view/TestRunnerView.php";
+	}else {
+		$target=PATH."server/view/MainView.php";
+	}
+} else {
+	$target=PATH."server/view/LoginView.php";
 }
-LoginController::main();
+}
+    die('Could not connect: ' . mysql_error());
+mysql_close($db_found);
+
+header("Location: $target");
+}
 ?>
