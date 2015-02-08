de0c93f38910e7f19f398dd1c5c1d54dbfabf934
Stefan Heid <stefan-heid@web.de>
2015-02-08 01:13:07 +0100

    Answer now reading db again
    AnswerPointController will put everything directly to database

--- server/controller/AnswerPointController.php
+++ server/controller/AnswerPointController.php
@@ -29,12 +29,13 @@ class AnswerPointController {
 	
 	
 	public static function main(){
-		$_POST['answer']
-		//initalize answerobjects from the answers
-		//calculate their points using the QuestionTable
-			//destinguish different kinds of questions
-		//put data to the db
-		
+		$answers = array();
+		//create new Test in DB
+		//create all the Answers in DB
+		// while calculating the points for closed and gap questions
+		foreach ($_POST['answer'] as $answer){
+			array_pop($answers,new Answer());
+		}
 		//calculate result
 		//redirect to after test view
 	}
--- server/model/Answer.php
+++ server/model/Answer.php
@@ -1,62 +1,53 @@
 <?php
-require_once(realpath(dirname(__FILE__)) . '/Question.php');
-require_once(realpath(dirname(__FILE__)) . '/Test.php');
+require_once (realpath ( dirname ( __FILE__ ) ) . '/Question.php');
+require_once (realpath ( dirname ( __FILE__ ) ) . '/Test.php');
 
 /**
+ *
  * @access public
  * @author gamer01
  * @package Server.Model
  */
 class Answer {
-	/**
-	 * @AttributeType int
-	 */
-	private $_iD;
+	private $_answerID;
+	private $_testID;
+	private $_questionID;
 	private $_answer;
-	/**
-	 * @AttributeType Integer
-	 */
 	private $_points;
-	/**
-	 * @AssociationType Server.Model.Question
-	 * @AssociationMultiplicity 1
-	 */
-	public $_for__;
-	/**
-	 * @AssociationType Server.Model.Test
-	 * @AssociationMultiplicity 1
-	 */
-	public $_unnamed_Test_;
-	/**
-	 * @AssociationType Server.Model.Evaluation Rule
-	 * @AssociationMultiplicity 1
-	 */
-	public $_evaluationRules;
-	/**
-	 * @AssociationType Server.Model.Question
-	 * @AssociationMultiplicity 1
-	 */
-	public $_questions;
-	
-	public function __construct() {
-		
+	public function __construct($id) {
+			$mysqli = DBController::getConnection ();
+			$result = $mysqli->query ( "SELECT * FROM Answer WHERE AnswerID=" . $id );
+			$row = $result->fetch_array ( MYSQLI_ASSOC );
+			
+			$this->_answerID = $row ['AnswerID'];
+			$this->_testID = new Test ( $row ['TestID'] );
+			$this->_questionID = new Question ( $row ['QuestionID'] );
+			$this->_answer = $row ['Answer'];
+			$this->_points = $row ['Points'];
+			
+			$result->close ();
 	}
-	
-	public function loafro
 	
 	//
 	public function getAnswer() {
 		return $this->_answer;
 	}
-/*	public static function getAnswer($id) {
-		$mysqli = DBController::getConnection ();
-		$result = $mysqli->query ( "SELECT Discriminator FROM Question WHERE AnswerID=" . $id );
-		$row = $result->fetch_array ( MYSQLI_ASSOC );
 	
-		
-		$result->close ();
-		$this->_solution = $row ['Solution'];
-		return $answer;
-	}*/
+	//
+	public function getQuestion() {
+		return $this->_questionID;
+	}
+	/*
+	 * public static function getAnswer($id) {
+	 * $mysqli = DBController::getConnection ();
+	 * $result = $mysqli->query ( "SELECT Discriminator FROM Question WHERE AnswerID=" . $id );
+	 * $row = $result->fetch_array ( MYSQLI_ASSOC );
+	 *
+	 *
+	 * $result->close ();
+	 * $this->_solution = $row ['Solution'];
+	 * return $answer;
+	 * }
+	 */
 }
 ?>
